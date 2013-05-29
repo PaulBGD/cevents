11e7c46bf9ebc5f8bf81155da833673d4a6938bb
ShadowLordAlpha <shadow.alpha70@gmail.com>
2013-05-29 15:40:43 -0600

    Create EventCTF.java
    Creates placeholder for Capture the Flag

---
+++ src/me/ultimate/Events/EventCTF.java
@@ -0,0 +1,14 @@
+/*
+* ************************************* Coded by Ultimate + ShadowLordAlpha *********************************
+* ANY use of this not on the server Coalition Events/Mini-Games is strictly PROHIBITED!
+* ***********************************************************************************************************
+*/
+package me.ultimate.Events;
+
+//imports here
+
+public class EventCTF {
+
+//placeholder for Capture the Flag
+
+}
