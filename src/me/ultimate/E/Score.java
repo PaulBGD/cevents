b0df556c884899318e977215146d49d7ee082ffb
ShadowLordAlpha <shadow.alpha70@gmail.com>
2013-05-29 16:04:27 -0600

    Create Score.java
    Score and almost full implementation should be used internally to create and get new scoreboards

---
+++ src/me/ultimate/E/Score.java
@@ -0,0 +1,46 @@
+/*
+* ************************************* Coded by Ultimate + ShadowLordAlpha *********************************
+* ANY use of this not on the server Coalition Events/Mini-Games is strictly PROHIBITED!
+* ***********************************************************************************************************
+*/
+package me.ultimate.E;
+
+//imports here, editing by hand so I have no idea what they are
+
+public class Score {
+
+  private Score impl;
+  private HashMap<String, String> scoreBoard = new HashMap<String, String>();
+
+  /**
+  *
+  *Used to start a new score
+  *
+  */
+  public Score() {
+    implementaion = this;
+  }
+  
+  public void addScore(String playerName, double score) {
+    if (scoreBoard.get(playerName) != null) {
+      scoreBoard.put(playerName, getScore(playerName) + score);
+    } else {
+      scoreBoard.put(playerName, score);
+    }
+  }
+  
+  public Score getImpl() {
+    return impl;
+  }
+  
+  /**
+  *
+  *Get the players score that is stored
+  *used mostly internaly
+  */
+  public double getScore(String playerName) {
+  
+    return scoreBoard.get(playerName);
+  }
+
+}
