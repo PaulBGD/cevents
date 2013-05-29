28a56a7e5b03008bbf47f83659205645c1d3d357
ShadowLordAlpha <shadow.alpha70@gmail.com>
2013-05-29 15:32:30 -0600

    Create EventPaintball.java
    create the placeholder for paintball

---
+++ src/me/ultimate/Events/EventPaintball.java
@@ -0,0 +1,16 @@
+/*
+* ************************************* Coded by Ultimate + ShadowLordAlpha *********************************
+* ANY use of this not on the server Coalition Events/Mini-Games is strictly PROHIBITED!
+* ***********************************************************************************************************
+*/
+
+package me.ultimate.Events;
+
+//imports here
+
+
+public class EventPaintball implements Listener {
+
+//does nothing for now as this is a possible placeholder
+
+}
